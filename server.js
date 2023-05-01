c0d47a5defe69fe97e3e058b8543614194f45db5
Cristofer Zamora <104957113+CristoferZR@users.noreply.github.com>
2023-04-30 21:39:47 -0600

    changes to mysql

--- server.js
+++ server.js
@@ -8,7 +8,7 @@
 
 
 const express = require('express');
-const mysql  = require('mysql');
+const mysql  = require('mysql2');
 const app =  express();
 const ejs = require('ejs');
 const bodyParser = require('body-parser');
